4caa7024ed76b64a4cc1e91a733553e6974e4a23
hu970804 <812510003@qq.com>
2018-01-26 16:27:56 +0800

    Promise

---
+++ src/Promise.js
@@ -0,0 +1,38 @@
+let state = 1;
+
+function step1(resolve, reject) {
+  console.log('1.洗菜做饭');
+  if(state == 1){
+    resolve('1...完成');
+  } else {
+    reject('1...没完成');
+  }
+}
+
+function step2(resolve, reject) {
+  console.log('2.吃饭');
+  if(state == 1){
+    resolve('2...完成');
+  } else {
+    reject('2...没完成');
+  }
+}
+
+function step3(resolve, reject) {
+  console.log('3.洗碗');
+  if(state == 1){
+    resolve('3...完成');
+  } else {
+    reject('3...没完成');
+  }
+}
+
+new Promise(step1).then( function(val){
+  console.log(val);
+  return new Promise(step2);
+}).then( function(val){
+  console.log(val);
+  return new Promise(step3);
+}).then( function(val) {
+  console.log(val);
+});
